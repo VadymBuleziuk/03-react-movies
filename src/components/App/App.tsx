2780fa3709194807b7518d0586bc400f7a9e9ee7
Vadym Buleziuk <666buzya@gmail.com>
2026-06-22 00:32:50 +0200

    refresh

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,13 @@
 import css from "./App.module.css";
 import SearchBar from "../SearchBar/SearchBar";
 import toast, { Toaster } from "react-hot-toast";
-import axios from "axios";
-import type { Movie } from "../types/movie";
+import type { Movie } from "../../types/movie";
 import { useState } from "react";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
-
-export interface MoviesResponse {
-  page: number;
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
-}
+import { fetchMovies } from "../../services/movieService";
 
 function App() {
   const [collection, setCollection] = useState<Movie[]>([]);
@@ -32,26 +25,17 @@ function App() {
     setError(false);
 
     try {
-      const response = await axios.get<MoviesResponse>(
-        "https://api.themoviedb.org/3/search/movie",
-        {
-          params: { query },
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      );
+      const movies = await fetchMovies(query, token);
 
-      if (response.data.total_results === 0) {
+      if (movies.length === 0) {
         toast.error("No movies found for your request.");
         setCollection([]);
         return;
       }
 
-      setCollection(response.data.results);
+      setCollection(movies);
     } catch {
       setError(true);
-      toast.error("Request failed");
     } finally {
       setShowLoader(false);
     }
